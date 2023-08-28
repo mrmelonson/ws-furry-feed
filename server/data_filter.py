5f5b8e973af637f41c58a70f7c38ad338be36a59
Zelenyy <dangerdude119@gmail.com>
2023-08-28 22:37:38 +1000

    Update all likes to accurate values when new post is seen

--- server/data_filter.py
+++ server/data_filter.py
@@ -1,7 +1,8 @@
 from atproto import models
-
+from atproto import Client
 from server.logger import logger
 from server.database import db, Post
+import json
 
 furfile =  open("./server/furries.txt", "r")
 furlist = furfile.read().split("\n")
@@ -14,6 +15,19 @@ filterfile.close()
 blacklistfile = open("./server/blacklist.txt", "r")
 blacklist = blacklistfile.read().split("\n")
 blacklistfile.close()
+
+with open('./server/secrets/secret.json') as file_object:
+        secrets = json.load(file_object)
+client = Client()
+client.login(secrets["Username"], secrets["Password"])
+
+def update_all_likes():
+    posts = Post.select()
+    for post in posts:
+        likes_accurate = client.bsky.feed.get_likes({'uri' : post.uri})
+        reposts_accurate = len(client.bsky.feed.get_reposted_by({'uri' : post.uri, 'limit' :100}))
+        Post.update(likes=likes_accurate, reposts=reposts_accurate).where(Post.uri == post.uri)
+
 
 def operations_callback(ops: dict) -> None:
     # Here we can filter, process, run ML classification, etc.
@@ -48,8 +62,14 @@ def operations_callback(ops: dict) -> None:
                     'cid': created_post['cid'],
                     'reply_parent': reply_parent,
                     'reply_root': reply_root,
+                    'likes' : 0,
+                    'views' : 0,
+                    'image' : post_with_images,
+                    'reposts' : 0
                 }
                 posts_to_create.append(post_dict)
+                update_all_likes()
+                
 
     posts_to_delete = [p['uri'] for p in ops['posts']['deleted']]
     if posts_to_delete:
@@ -61,3 +81,35 @@ def operations_callback(ops: dict) -> None:
             for post_dict in posts_to_create:
                 Post.create(**post_dict)
         logger.info(f'Added to feed: {len(posts_to_create)}')
+
+    for liked_post in ops['likes']['created']:
+        record = liked_post['record']
+        try:
+            liked_post_to_update = Post.get(Post.uri == record.subject.uri)
+            Post.update(likes=Post.likes + 1).where(Post.uri == record.subject.uri).execute()
+            print(Post.get(Post.uri == record.subject.uri).likes)
+            logger.info(f'Updated post likes (added): {record.subject.uri}')     
+        except:
+            next
+
+    #figure out how to remove likes from posts
+    posts_to_unlike = [p['uri'] for p in ops['likes']['deleted']]
+    if posts_to_unlike:
+        Post.update(likes=Post.likes -1).where(Post.uri.in_(posts_to_delete))
+        print("removed like")
+        #print(liked_post_removed)
+        '''
+        try:
+            liked_post_removed_to_update = Post.get(Post.uri == record.subject.uri)
+            Post.update(likes=Post.likes - 1).where(Post.uri == record.subject.uri)
+            logger.info(f'Updated post likes (removed): {record.subject.uri}')     
+        except:
+            next
+        '''
+        #if liked_posts_to_update:
+        #    for uri in liked_posts_to_update:
+        #        Post.update(likes=Post.likes + 1).where(Post.uri == uri)
+         #       logger.info(f'Updated post likes: {uri}')     
+
+
+
