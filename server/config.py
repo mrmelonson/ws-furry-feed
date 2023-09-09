6ffdad48b8e141c54bb3d44225aae3675eb3b052
zelenyy <dangerdude119@gmail.com>
2023-09-09 16:40:49 +1000

    no need to add environment variables

--- server/config.py
+++ server/config.py
@@ -1,7 +1,13 @@
 import os
+import json
+
+with open('./server/secrets/static.json') as file_object:
+    static_values = json.load(file_object)
 
 SERVICE_DID = os.environ.get('SERVICE_DID', None)
-HOSTNAME = os.environ.get('HOSTNAME', None)
+#HOSTNAME = os.environ.get('HOSTNAME', None)
+
+HOSTNAME = static_values["HOSTNAME"]
 
 if HOSTNAME is None:
     raise RuntimeError('You should set "HOSTNAME" environment variable first.')
@@ -10,7 +16,9 @@ if SERVICE_DID is None:
     SERVICE_DID = f'did:web:{HOSTNAME}'
 
 
-PISS_ALGO_URI = os.environ.get('PISS_ALGO_URI')
+#PISS_ALGO_URI = os.environ.get('PISS_ALGO_URI')
+PISS_ALGO_URI = static_values["PISS_URI"]
+
 if PISS_ALGO_URI is None:
     raise RuntimeError('Publish your feed first (run publish_feed.py) to obtain Feed URI. '
                        'Set this URI to "PISS_ALGO_URI" environment variable.')
