9db6efdde70f07814b23796e2c43e5f14f7ded65
Zelenyy <dangerdude119@gmail.com>
2023-09-28 21:41:27 +1000

    Patch to fix new atproto bug

--- server/data_filter.py
+++ server/data_filter.py
@@ -8,6 +8,30 @@ import re
 
 from datetime import datetime, timedelta
 
+'''
+TEMP PATCH REMOVE WHEN ATPROTO UPDATES
+'''
+from atproto.xrpc_client.models.com.atproto.server import create_session
+from atproto.xrpc_client.models import base
+from pydantic import Field
+import typing as t
+
+class Response(base.ResponseModelBase):
+
+    """Output data model for :obj:`com.atproto.server.createSession`."""
+
+    access_jwt: str = Field(alias='accessJwt')  #: Access jwt.
+    did: str  #: Did.
+    handle: str  #: Handle.
+    refresh_jwt: str = Field(alias='refreshJwt')  #: Refresh jwt.
+    email: t.Optional[str] = None  #: Email.
+    emailConfirmed: t.Optional[bool] = None
+
+create_session.Response = Response
+'''
+TEMP PATCH REMOVE WHEN ATPROTO UPDATES
+'''
+
 furfile =  open("./server/furries.txt", "r")
 furlist = furfile.read().split("\n")
 furfile.close()
